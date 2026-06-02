5b8434c4e3b3fa4a0d7a49f6482dc67d821b1749
saintmob <saintmob@gmail.com>
2026-06-02 14:09:08 +0800

    Add Cloudflare route return bridge

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { LanguageProvider } from './store/LanguageContext';
+import { startScreenRouteReturnBridge } from './lib/screenRouteReturnBridge';
+
+startScreenRouteReturnBridge();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
